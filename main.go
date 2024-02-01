ff4e273bfb7deb85fbe047097743e466128cb43c
seanschen <405621584@qq.com>
2024-02-01 19:39:16 +0800

    add main

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"log/slog"
+	"os"
+)
 
+// main Entrance
+func main() {
+	log := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	log.Info("log test", "user", 1)
 }
